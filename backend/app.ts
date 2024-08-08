6009f462c06fac748a25a001bb681f0b54eda45d
Max <48150531+mashtakovmm@users.noreply.github.com>
2024-08-08 19:30:40 +0300

    setup db

---
+++ backend/app.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import path from 'path';
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config({ path: path.resolve(`${__dirname}/../.env`) });
+
+const BACKEND_PORT = process.env.BACKEND_PORT || 8000;
+const SECRET = process.env.JWT_SECRET;
+
+if (!SECRET) {
+    throw new Error('JWT SECRET is missing in .env file');
+}
+
+const DATABASE_ROOT = process.env.DATABASE_ROOT || 'postgres';
+const DATABASE_PASS = process.env.DATABASE_PASS || 'password';
+const DATABASE_NAME = process.env.DATABASE_NAME || "mydb";
+const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost';
+
+const connectionURL = `postgresql://${DATABASE_ROOT}:${DATABASE_PASS}@D${DATABASE_HOST}:5432/${DATABASE_NAME}?schema=public`;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Books API")
+});
+
+app.post("/users/login", (req, res) => {
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+        },
+        SECRET,
+        { expiresIn: '1h' }
+    );
+
+    res.json({
+        succcess: true,
+        token: token
+    })
+})
+
+const start = async () => {
+    try {
+        app.listen(BACKEND_PORT, () => {
+            console.log(`Server is listening on port ${BACKEND_PORT}...`);
+        }).on("error", (err) => console.log(`Error; ${err}`));
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+start();
